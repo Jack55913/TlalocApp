a3f65109b21584a9e97eb02a4ac95531c0bfb74b
Gabriel <gabrc52@gmail.com>
2022-06-22 21:20:10 -0500

    QR Code.

--- tlaloc/lib/onboarding/common_select.dart
+++ tlaloc/lib/onboarding/common_select.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tlaloc/models/app_state.dart';
 import 'package:tlaloc/models/constants.dart';
 import 'package:tlaloc/screens/home.dart';
@@ -83,6 +85,13 @@ class CommonSelectPage extends StatelessWidget {
   }
 }
 
+void _goHome(BuildContext context) {
+  Navigator.of(context).pushAndRemoveUntil(
+      MaterialPageRoute<void>(builder: (BuildContext context) {
+    return const HomePage();
+  }), (Route<dynamic> route) => false);
+}
+
 class CommonSelectWidget extends StatelessWidget {
   final String ejido;
   final int hectareas;
@@ -96,56 +105,54 @@ class CommonSelectWidget extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: InkWell(
-          onTap: () async {
-            Navigator.of(context).pushAndRemoveUntil(
-                MaterialPageRoute<void>(builder: (BuildContext context) {
-              return const HomePage();
-            }), (Route<dynamic> route) => false);
-            final state = Provider.of<AppState>(context, listen: false);
-            state.changeEjido(ejido);
-          },
-          child: Container(
-            width: MediaQuery.of(context).size.width * 0.7,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(25.0),
-              color: Colors.white,
+        onTap: () async {
+          _goHome(context);
+          final state = Provider.of<AppState>(context, listen: false);
+          state.changeEjido(ejido);
+        },
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.7,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25.0),
+            color: Colors.white,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              children: [
+                // Insert circle image here:
+                CircleAvatar(
+                  backgroundColor: AppColors.orange1,
+                  radius: 50,
+                  child: Icon(
+                    FontAwesomeIcons.t,
+                    color: Colors.white,
+                    size: 50,
+                  ),
+                ),
+                SizedBox(height: 5),
+                Text(
+                  ejido,
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.black,
+                    fontFamily: 'FredokaOne',
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  '$hectareas Ha',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    fontFamily: 'poppins',
+                  ),
+                ),
+              ],
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: Column(
-                children: [
-                  // Insert circle image here:
-                  CircleAvatar(
-                    backgroundColor: AppColors.orange1,
-                    radius: 50,
-                    child: Icon(
-                      FontAwesomeIcons.t,
-                      color: Colors.white,
-                      size: 50,
-                    ),
-                  ),
-                  SizedBox(height: 5),
-                  Text(
-                    ejido,
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.black,
-                      fontFamily: 'FredokaOne',
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    '$hectareas Ha',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.black,
-                      fontFamily: 'poppins',
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
@@ -153,59 +160,115 @@ class CommonSelectWidget extends StatelessWidget {
 class QrSelectWidget extends StatelessWidget {
   const QrSelectWidget({Key? key}) : super(key: key);
 
-// String qrValue = "Codigo Qr";
-
-// void scanQr() async {
-//   String cameraScanResult = await scanner.scan();
-//   setState(() {
-//     qrValue = cameraScanResult;
-//   });
-// }
-
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: InkWell(
-          // onPressed: () => scanQr(),
-          child: Container(
-        width: MediaQuery.of(context).size.width * 0.7,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(25.0),
-          color: Colors.white,
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: Column(
-            children: [
-              // Insert circle image here:
-              CircleAvatar(
-                backgroundColor: Colors.white,
-                backgroundImage: AssetImage('assets/images/qr_code.png'),
-                radius: 50,
-              ),
-              SizedBox(height: 5),
-              Text(
-                'QR',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.black,
-                  fontFamily: 'FredokaOne',
+        onTap: () async {
+          final qrResult = await showDialog<String>(
+            context: context,
+            builder: (context) {
+              final size = min(MediaQuery.of(context).size.width,
+                  MediaQuery.of(context).size.height);
+              return Dialog(
+                child: SizedBox(
+                  height: size,
+                  width: size,
+                  child: MobileScanner(
+                    onDetect: (barcode, args) {
+                      Navigator.pop(context, barcode.rawValue);
+                    },
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                'Detecta tu pluviómetro automáticamente',
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Colors.black,
-                  fontFamily: 'poppins',
+              );
+            },
+          );
+          if (qrResult != null) {
+            String? ejido;
+            if (qrResult.contains('tlaloc.web.app')) {
+              ejido = qrResult
+                  .split('/')
+                  .last
+                  .replaceAll('_', ' ')
+                  .replaceAll('%20', ' ');
+            }
+            if (ejido == null || ejido == '') {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: Text('El código QR no es válido'),
+                  actions: [
+                    TextButton(
+                      child: Text('De acuerdo'),
+                      onPressed: () => Navigator.of(context).pop(),
+                    ),
+                  ],
                 ),
+                barrierDismissible: true,
+              );
+            } else {
+              _goHome(context);
+              final state = Provider.of<AppState>(context, listen: false);
+              state.changeEjido(ejido);
+            }
+          } else {
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: Text('No escaneaste ningún código QR'),
+                content:
+                    Text('Intenta de nuevo o selecciona tu ejido manualmente'),
+                actions: [
+                  TextButton(
+                    child: Text('De acuerdo'),
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                ],
               ),
-            ],
+              barrierDismissible: true,
+            );
+          }
+        },
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.7,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25.0),
+            color: Colors.white,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              children: [
+                // Insert circle image here:
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  backgroundImage: AssetImage('assets/images/qr_code.png'),
+                  radius: 50,
+                ),
+                SizedBox(height: 5),
+                Text(
+                  'QR',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.black,
+                    fontFamily: 'FredokaOne',
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Detecta tu pluviómetro automáticamente',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.black,
+                    fontFamily: 'poppins',
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-      )),
+      ),
     );
   }
 }
